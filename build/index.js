dbbef045572d6cfc045509deaec639802d39c145
Attila Domokos <adomokos@gmail.com>
2020-09-01 09:45:26 -0500

    Add a couple of examples

--- build/index.js
+++ build/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('Hello world!');
+var Greeter = /** @class */ (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
+var greeter = new Greeter("world");
+console.log(greeter.greet());
+function getFinalPrice(price, discount) {
+    return price - price / discount;
+}
+console.log(getFinalPrice(100, 10));
+// console.log(getFinalPrice(100, "10%")); // type error
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello world!')
+class Greeter {
+  greeting: string;
+
+  constructor(message: string) {
+    this.greeting = message;
+  }
+
+  greet() {
+    return "Hello, " + this.greeting;
+  }
+}
+
+const greeter = new Greeter("world");
+
+console.log(greeter.greet());
+
+function getFinalPrice(price: number, discount: number) {
+  return price - price/discount;
+}
+
+console.log(getFinalPrice(100, 10));
+// console.log(getFinalPrice(100, "10%")); // type error
