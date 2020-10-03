bf0500929cbea3a0fc2d1d2a3f91c612277405c3
Attila Domokos <adomokos@gmail.com>
2020-10-03 14:09:06 -0500

    Extract a class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-class Greeter {
-  greeting: string;
-
-  constructor(message: string) {
-    this.greeting = message;
-  }
-
-  greet() {
-    return "Hello, " + this.greeting;
-  }
-}
+import Greeter from './greeter'
 
 const greeter = new Greeter("world");
 
